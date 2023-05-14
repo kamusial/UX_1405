6b7a20f1ee97c111be98d62e7969d676b0c3af00
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-14 14:00:38 +0200

    1

--- 7_drawing.py
+++ 7_drawing.py
@@ -9,13 +9,15 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.graphics import Color
 from kivy.graphics import Rectangle
-
+from kivy.graphics import Line
 
 class Touch(Widget):
     def __init__(self, **kwargs):
         super(Touch, self).__init__(**kwargs)
 
         with self.canvas:
+            Color(1, 0, 0, 0.7, mode='rgba')
+            Line(points=(20,30,400,300,60,500,20,30))
             Color(1, 0, 0, 0.7, mode='rgba')
             self.rect1 = Rectangle(pos=(200,100), size=(80,90))
             Color(0, 1, 0, 0.7, mode='rgba')
@@ -28,12 +30,16 @@ class Touch(Widget):
         print('Mysz wcisnieta',touch.pos)
         self.btn.opacity = 0.3
         self.rect1.pos = touch.pos
+        self.original_button_pos = self.btn.pos
+        self.btn.pos = touch.pos
+        self.btn.size[0] += 10
     def on_touch_move(self, touch):
         print('Ruch myszą', touch)
-        self.rect2.pos = touch.pos
+        self.rect2.pos = (touch.pos[0]-40, touch.pos[1]-150)
     def on_touch_up(self, touch):
         print('mysz odcisnieta',touch.spos)
         self.btn.opacity = 1
+        self.btn.pos[1] = self.btn.pos[1] + 30
 
 
 class My6App(App):
--- 8_multi_windows.py
+++ 8_multi_windows.py
@@ -10,11 +10,24 @@ from kivy.properties import ObjectProperty
 from kivy.graphics import Color
 from kivy.graphics import Rectangle
 from kivy.graphics import Line
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class MyApp(App):
+
+class MainWindow(Screen):
+    pass
+
+class SecondWindow(Screen):
+    pass
+
+class WindowsManager(ScreenManager):
+
+kv = Builder.load_file('my8.kv')
+
+class MyMainApp(App):
     def build(self):
-        return
+        return kv
 
 
 if __name__ == "__main__":
-    MyApp().run()
+    MyMainApp().run()
