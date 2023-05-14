615418001e79f8926c9bc1b21f5338f116081f1b
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-14 09:44:17 +0200

    1

---
+++ 3_button.py
@@ -0,0 +1,39 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        self.inside.add_widget(Label(text="Imie:  "))
+        self.imie = TextInput(multiline=False)
+        self.inside.add_widget(self.imie)
+
+        self.inside.add_widget(Label(text="Nazwisko:  "))
+        self.nazwisko = TextInput(multiline=False)
+        self.inside.add_widget(self.nazwisko)
+
+        self.inside.add_widget(Label(text="wiek:  "))
+        self.wiek = TextInput(multiline=False)
+        self.inside.add_widget(self.wiek)
+
+        self.add_widget(self.inside)
+        self.submit = Button(text="Wyslij", font_size=40)
+        self.add_widget(self.submit)
+
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == "__main__":
+    MyApp().run()
