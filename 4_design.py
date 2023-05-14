36688918dd02d7ed42ddb36f444b2c9b9085ed62
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-14 10:43:19 +0200

    1

--- 4_design.py
+++ 4_design.py
@@ -4,11 +4,15 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
-class MyApp(App):
+class MyGrid(Widget):
+    pass
+
+class My4App(App):
     def build(self):
-        return
+        return MyGrid()
 
 
 if __name__ == "__main__":
-    MyApp().run()
+    My4App().run()
