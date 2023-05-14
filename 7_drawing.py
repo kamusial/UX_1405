583306553fd075ef7e853ae7c17d4af49c2d4d43
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-14 13:15:44 +0200

    1

--- 7_drawing.py
+++ 7_drawing.py
@@ -7,8 +7,19 @@ from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.graphics import Color
+from kivy.graphics import Rectangle
+
 
 class Touch(Widget):
+    def __init__(self, **kwargs):
+        super(Touch, self).__init__(**kwargs)
+
+        with self.canvas:
+            Color(1, 0, 0, 0.7, mode='rgba')
+            self.rect1 = Rectangle(pos=(0,0), size=(80,300))
+
+
     def on_touch_down(self, touch):
         print('Mysz wcisnieta',touch.pos)
         self.btn.opacity = 0.3
